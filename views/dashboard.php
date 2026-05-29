85447ab50bde913e8e421dcd1589560d4bfdb251
Andres Cardenas <andrescardenase77@gmail.com>
2026-05-28 20:41:14 -0500

    changes

--- views/dashboard.php
+++ views/dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['isAuthenticated']) || $_SESSION['isAuthenticated'] !== true) {
-    header("Location: unauthorized.php");
+    header("Location: unauthorized.html");
     exit();
 }
 ?>
